833996e8ee84f2093996939ac0e4b75f2ab49b00
mike kimani <kimanimichael09@gmail.com>
2025-04-09 00:27:58 +0300

    loading files into main.py

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ import nltk
 
 from nltk.stem import WordNetLemmatizer
 from keras.models import load_model
+
+# initializing the lemmatizer
+lemmatizer = WordNetLemmatizer()
+nltk.download('punkt')
+
+# loading the files that were saved in the training.py file
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.h5')
