f56b751aafe228a7ceb177c2e671288941efb392
mike kimani <kimanimichael09@gmail.com>
2025-04-09 00:25:27 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# # This main.py file will be used to run the model 
-# import random 
-# import json
-# import pickle
-# import numpy as np
-# import nltk
+# This main.py file will be used to run the model 
+import random 
+import json
+import pickle
+import numpy as np
+import nltk
 
-# from nltk.stem import WordNetLemmatizer
-# from keras.models import load_model
+from nltk.stem import WordNetLemmatizer
+from keras.models import load_model
