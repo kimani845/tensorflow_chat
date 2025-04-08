76f97719b68bdb809f830383382afedf373df063
mike kimani <kimanimichael09@gmail.com>
2025-04-09 00:32:23 +0300

    Separating words from sentences given as inputs

--- main.py
+++ main.py
@@ -17,3 +17,8 @@ intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 model = load_model('chatbot_model.h5')
+
+# Function to separate words from the sentences given as inputs
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence) # Tokenizing the sentence
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words] # Lemmatizing the words in the sentence
